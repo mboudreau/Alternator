97eb5291b9d839ebe7de50c887c138cd5bc06500
Michel Boudreau <michelboudreau@gmail.com>
2012-03-21 15:05:20 -0400

    First commit - setting up project structure and maven

---
+++ src/main/java/com.michelboudreau.alternator/Alternator.java
@@ -0,0 +1,15 @@
+package com.michelboudreau.alternator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mboudreau
+ * Date: 3/21/12
+ * Time: 12:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Alternator {
+
+	public void main() {
+		
+	}
+}
