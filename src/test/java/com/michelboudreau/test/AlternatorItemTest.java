f9d63591eb4421925645fa2312f68b5efec22564
Michel Boudreau <michelboudreau@gmail.com>
2012-05-17 19:29:17 -0400

    Removing some commented out tests

--- src/test/java/com/michelboudreau/test/AlternatorItemTest.java
+++ src/test/java/com/michelboudreau/test/AlternatorItemTest.java
@@ -24,6 +24,8 @@ public class AlternatorItemTest extends AlternatorTest {
 		deleteAllTables();
 	}
 
+	// TODO: need to test for different schemas
+
 	@Test
 	public void putItemWithHashKey() {
 		KeySchema schema = new KeySchema(new KeySchemaElement().withAttributeName("id").withAttributeType(ScalarAttributeType.S));
@@ -156,71 +158,7 @@ public class AlternatorItemTest extends AlternatorTest {
 	public void batchGetItemInTableWithoutRequestItemsTest() {
 		Assert.assertNull(client.batchGetItem(new BatchGetItemRequest()).getResponses());
 	}
-
-	@Test
-	public void deleteItemWithoutTableNameTest() {
-		DeleteItemRequest delete = new DeleteItemRequest();
-		delete.setKey(getHashKey());
-		Assert.assertNull(client.deleteItem(delete).getAttributes());
-	}
-
-	@Test
-	public void deleteItemWithoutKeyTest() {
-		DeleteItemRequest delete = new DeleteItemRequest();
-		delete.setTableName(testTableName);
-		Assert.assertNull(client.deleteItem(delete).getAttributes());
-	}
-
-
-
-	@Test
-	public void putItemWithoutTableNameTest() {
-		PutItemRequest req = new PutItemRequest();
-		req.setItem(generateStaticItem());
-		Assert.assertNull(client.putItem(req).getAttributes());
-	}
-
-	@Test
-	public void putItemWithoutitemTest() {
-		PutItemRequest req = new PutItemRequest();
-		req.setTableName(testTableName);
-		Assert.assertNull(client.putItem(req).getAttributes());
-	}
-
-	@Test
-	public void updateItemWithoutTableNameTest() {
-		UpdateItemRequest req = new UpdateItemRequest();
-		req.setKey(getHashKey());
-		Assert.assertNull(client.updateItem(req).getAttributes());
-	}
-
-	@Test
-	public void updateItemWithoutKeyTest() {
-		UpdateItemRequest req = new UpdateItemRequest();
-		req.setTableName(testTableName);
-		Assert.assertNull(client.updateItem(req).getAttributes());
-	}*/
-
-/*
-	public BatchGetItemRequest generateGetBatchRequest() {
-		BatchGetItemRequest batchGetItemRequest = new BatchGetItemRequest();
-		Map<String, KeysAndAttributes> requestItems = new HashMap<String, KeysAndAttributes>();
-		Key table1key1 = new Key().withHashKeyElement(new AttributeValue().withS("123"));
-		requestItems.put(testTableName, new KeysAndAttributes().withKeys(table1key1));
-		batchGetItemRequest.withRequestItems(requestItems);
-		return batchGetItemRequest;
-	}
-
-	public BatchWriteItemRequest generateWriteBatchRequest() {
-		BatchWriteItemRequest request = new BatchWriteItemRequest();
-		Map<String, List<WriteRequest>> requestItems = new HashMap<String, List<WriteRequest>>();
-		List<WriteRequest> itemList = new ArrayList<WriteRequest>();
-		itemList.add(new WriteRequest().withPutRequest(new PutRequest().withItem(generateStaticItem())));
-		itemList.add(new WriteRequest().withPutRequest(new PutRequest().withItem(generateNewStaticItem())));
-		requestItems.put(testTableName, itemList);
-		request.setRequestItems(requestItems);
-		return request;
-	}*/
+*/
 
 	protected AttributeValue createStringAttribute() {
 		return new AttributeValue(UUID.randomUUID().toString());
