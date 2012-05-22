eca47482166ac6829eb4dcd8b431029a1371d034
Michel Boudreau <michelboudreau@gmail.com>
2012-05-22 09:29:01 -0400

    Latest commit for thomas to work on

---
+++ src/main/java/com/amazonaws/services/dynamodb/model/transform/QueryResultMarshaller.java
@@ -0,0 +1,88 @@
+package com.amazonaws.services.dynamodb.model.transform;
+
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.dynamodb.model.AttributeValue;
+import com.amazonaws.services.dynamodb.model.Key;
+import com.amazonaws.services.dynamodb.model.QueryResult;
+import com.amazonaws.transform.Marshaller;
+import com.amazonaws.util.json.JSONWriter;
+import org.springframework.util.StringUtils;
+
+import java.io.StringWriter;
+import java.util.Map;
+
+public class QueryResultMarshaller implements Marshaller<String, QueryResult> {
+
+	public String marshall(QueryResult queryResult) {
+		if (queryResult == null) {
+			throw new AmazonClientException("Invalid argument passed to marshall(...)");
+		}
+
+		try {
+			StringWriter stringWriter = new StringWriter();
+			JSONWriter jsonWriter = new JSONWriter(stringWriter);
+			jsonWriter.object();
+
+			if (queryResult.getCount() != null) {
+				jsonWriter.key("Count").value(queryResult.getCount());
+			}
+			if (queryResult.getItems() != null) {
+				jsonWriter.key("Items").array();
+				for (Map<String, AttributeValue> value : queryResult.getItems()) {
+					jsonWriter.object();
+					for (Map.Entry<String, AttributeValue> item : value.entrySet()) {
+						String key = item.getKey();
+						AttributeValue val = item.getValue();
+						jsonWriter.key(key).object();
+						if (val.getN() != null) {
+							jsonWriter.key("N").value(val.getN());
+						} else if (val.getS() != null) {
+							jsonWriter.key("S").value(val.getS());
+						} else if (val.getNS() != null) {
+							jsonWriter.key("NS").value(StringUtils.collectionToCommaDelimitedString(val.getNS()));
+						} else if (val.getSS() != null) {
+							jsonWriter.key("SS").value(StringUtils.collectionToCommaDelimitedString(val.getSS()));
+						}
+						jsonWriter.endObject();
+					}
+					jsonWriter.endObject();
+				}
+				jsonWriter.endArray();
+			}
+			if (queryResult.getLastEvaluatedKey() != null) {
+				Key key = queryResult.getLastEvaluatedKey();
+				jsonWriter.key("LastEvaluatedKey").object();
+				AttributeValue value;
+				if (key.getHashKeyElement() != null) {
+					jsonWriter.key("HashKeyElement").object();
+					value = key.getHashKeyElement();
+					if (value.getN() != null) {
+						jsonWriter.key(value.getN()).value("N");
+					} else if (value.getS() != null) {
+						jsonWriter.key(value.getS()).value("S");
+					}
+					jsonWriter.endObject();
+				}
+				if (key.getRangeKeyElement() != null) {
+					jsonWriter.key("RangeKeyElement").object();
+					value = key.getRangeKeyElement();
+					if (value.getN() != null) {
+						jsonWriter.key(value.getN()).value("N");
+					} else if (value.getS() != null) {
+						jsonWriter.key(value.getS()).value("S");
+					}
+					jsonWriter.endObject();
+				}
+				jsonWriter.endObject();
+			}
+			if (queryResult.getConsumedCapacityUnits() != null) {
+				jsonWriter.key("ConsumedCapacityUnits").value(queryResult.getConsumedCapacityUnits());
+			}
+			jsonWriter.endObject();
+			return stringWriter.toString();
+		} catch (Throwable t) {
+			throw new AmazonClientException("Unable to marshall request to JSON: " + t.getMessage(), t);
+		}
+	}
+}
---
+++ src/main/java/com/michelboudreau/alternator/validators/ConditionValidator.java
@@ -0,0 +1,31 @@
+package com.michelboudreau.alternator.validators;
+
+import com.amazonaws.services.dynamodb.model.AttributeValue;
+import com.amazonaws.services.dynamodb.model.ComparisonOperator;
+import com.amazonaws.services.dynamodb.model.Condition;
+import com.michelboudreau.alternator.validation.Validator;
+import com.michelboudreau.alternator.validation.ValidatorUtils;
+
+import java.util.List;
+
+public class ConditionValidator extends Validator {
+
+	public Boolean supports(Class clazz) {
+		return Condition.class.isAssignableFrom(clazz);
+	}
+
+	public List<Error> validate(Object target) {
+		Condition instance = (Condition) target;
+		List<Error> errors = ValidatorUtils.rejectIfNull(instance);
+		if (errors.size() == 0) {
+			errors.addAll(ValidatorUtils.rejectIfNullOrEmptyOrWhitespace(instance.getComparisonOperator()));
+			errors.addAll(ValidatorUtils.rejectIfNotMatchRegex(instance.getComparisonOperator(), "^(EQ|NE|IN|LE|LT|GE|GT|BETWEEN|NOT_NULL|NULL|CONTAINS|NOT_CONTAINS|BEGINS_WITH)$"));
+			if(instance.getAttributeValueList() != null) {
+				for(AttributeValue value:instance.getAttributeValueList()) {
+					errors.addAll(ValidatorUtils.invokeValidator(new PrimaryKeyValidator(), value));
+				}
+			}
+		}
+		return removeNulls(errors);
+	}
+}
