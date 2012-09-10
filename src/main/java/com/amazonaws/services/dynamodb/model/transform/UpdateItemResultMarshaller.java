7a2fc196edccf4e5c726ad49ecd2d193d8160425
Thomas Bredillet <tnbredillet@gmail.com>
2012-09-10 16:56:18 -0400

    fixed a problem with the UpdateItemResult unmarshaller

--- src/main/java/com/amazonaws/services/dynamodb/model/transform/UpdateItemResultMarshaller.java
+++ src/main/java/com/amazonaws/services/dynamodb/model/transform/UpdateItemResultMarshaller.java
@@ -10,35 +10,37 @@ import java.io.StringWriter;
 
 public class UpdateItemResultMarshaller implements Marshaller<String, UpdateItemResult> {
 
-    public String marshall(UpdateItemResult updateItemResult) {
-        if (updateItemResult == null) {
-            throw new AmazonClientException("Invalid argument passed to marshall(...)");
-        }
-        try {
-            StringWriter stringWriter = new StringWriter();
-            JSONWriter jsonWriter = new JSONWriter(stringWriter);
-            jsonWriter.object();
-            jsonWriter.key("Attributes").object();
-            for (String key : updateItemResult.getAttributes().keySet()) {
-                jsonWriter.key(key).object();
-                if(updateItemResult.getAttributes().get(key).getS()!=null){
-                    jsonWriter.key("S").value(updateItemResult.getAttributes().get(key).getS());
-                } else if(updateItemResult.getAttributes().get(key).getN()!=null){
-                    jsonWriter.key("N").value(updateItemResult.getAttributes().get(key).getN());
-                } else if(updateItemResult.getAttributes().get(key).getSS()!=null){
-                    jsonWriter.key("SS").value(updateItemResult.getAttributes().get(key).getSS());
-                }  else if(updateItemResult.getAttributes().get(key).getNS()!=null){
-                    jsonWriter.key("NS").value(updateItemResult.getAttributes().get(key).getNS());
-                }
-                jsonWriter.endObject();
-            }
-            jsonWriter.endObject();
-            jsonWriter.key("ConsumedCapacityUnits").value(updateItemResult.getConsumedCapacityUnits());
-            jsonWriter.endObject();
+	public String marshall(UpdateItemResult updateItemResult) {
+		if (updateItemResult == null) {
+			throw new AmazonClientException("Invalid argument passed to marshall(...)");
+		}
+		try {
+			StringWriter stringWriter = new StringWriter();
+			JSONWriter jsonWriter = new JSONWriter(stringWriter);
+			jsonWriter.object();
+			jsonWriter.key("Attributes").object();
+			for (String key : updateItemResult.getAttributes().keySet()) {
+				jsonWriter.key(key).object();
+				if (updateItemResult.getAttributes().get(key) != null) {
+					if (updateItemResult.getAttributes().get(key).getS() != null) {
+						jsonWriter.key("S").value(updateItemResult.getAttributes().get(key).getS());
+					} else if (updateItemResult.getAttributes().get(key).getN() != null) {
+						jsonWriter.key("N").value(updateItemResult.getAttributes().get(key).getN());
+					} else if (updateItemResult.getAttributes().get(key).getSS() != null) {
+						jsonWriter.key("SS").value(updateItemResult.getAttributes().get(key).getSS());
+					} else if (updateItemResult.getAttributes().get(key).getNS() != null) {
+						jsonWriter.key("NS").value(updateItemResult.getAttributes().get(key).getNS());
+					}
+				}
+				jsonWriter.endObject();
+			}
+			jsonWriter.endObject();
+			jsonWriter.key("ConsumedCapacityUnits").value(updateItemResult.getConsumedCapacityUnits());
+			jsonWriter.endObject();
 
-            return stringWriter.toString();
-        } catch (Throwable t) {
-            throw new AmazonClientException("Unable to marshall request to JSON: " + t.getMessage(), t);
-        }
-    }
+			return stringWriter.toString();
+		} catch (Throwable t) {
+			throw new AmazonClientException("Unable to marshall request to JSON: " + t.getMessage(), t);
+		}
+	}
 }
