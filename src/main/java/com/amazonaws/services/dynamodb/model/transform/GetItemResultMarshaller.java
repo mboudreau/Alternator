2be11e9683ce55ba2b405273a0a1141008420511
Thomas Bredillet <tnbredillet@gmail.com>
2012-09-11 16:27:44 -0400

    fixed a bug in the getitemresult marshaller

--- src/main/java/com/amazonaws/services/dynamodb/model/transform/GetItemResultMarshaller.java
+++ src/main/java/com/amazonaws/services/dynamodb/model/transform/GetItemResultMarshaller.java
@@ -15,34 +15,36 @@ public class GetItemResultMarshaller implements Marshaller<String, GetItemResult
 			throw new AmazonClientException("Invalid argument passed to marshall(...)");
 		}
 
-        try {
-            StringWriter stringWriter = new StringWriter();
-            JSONWriter jsonWriter = new JSONWriter(stringWriter);
-            jsonWriter.object();
+		try {
+			StringWriter stringWriter = new StringWriter();
+			JSONWriter jsonWriter = new JSONWriter(stringWriter);
+			jsonWriter.object();
 
-            if (getItemResult.getItem() != null) {
-                jsonWriter.key("Item").object();
-                for(String key : getItemResult.getItem().keySet()) {
-                    AttributeValue value = getItemResult.getItem().get(key);
-                    jsonWriter.key(key).object();
-                    if(value.getN()!=null){
-                        jsonWriter.key("N").value(value.getN());
-                    }else if(value.getS()!=null){
-                        jsonWriter.key("S").value(value.getS());
-                    } else if(value.getSS()!=null){
-                        jsonWriter.key("SS").value(value.getSS());
-                    }else if(value.getNS()!=null){
-                        jsonWriter.key("NS").value(value.getNS());
-                    }
-                    jsonWriter.endObject();
-                }
-                jsonWriter.endObject();
-            }
-            jsonWriter.key("ConsumedCapacityUnits").value(0.5);
-            jsonWriter.endObject();
-            return stringWriter.toString();
-        } catch (Throwable t) {
-            throw new AmazonClientException("Unable to marshall request to JSON: " + t.getMessage(), t);
-        }
+			if (getItemResult.getItem() != null) {
+				jsonWriter.key("Item").object();
+				for (String key : getItemResult.getItem().keySet()) {
+					AttributeValue value = getItemResult.getItem().get(key);
+					if (value != null) {
+						jsonWriter.key(key).object();
+						if (value.getN() != null) {
+							jsonWriter.key("N").value(value.getN());
+						} else if (value.getS() != null) {
+							jsonWriter.key("S").value(value.getS());
+						} else if (value.getSS() != null) {
+							jsonWriter.key("SS").value(value.getSS());
+						} else if (value.getNS() != null) {
+							jsonWriter.key("NS").value(value.getNS());
+						}
+						jsonWriter.endObject();
+					}
+				}
+				jsonWriter.endObject();
+			}
+			jsonWriter.key("ConsumedCapacityUnits").value(0.5);
+			jsonWriter.endObject();
+			return stringWriter.toString();
+		} catch (Throwable t) {
+			throw new AmazonClientException("Unable to marshall request to JSON: " + t.getMessage(), t);
+		}
 	}
 }
