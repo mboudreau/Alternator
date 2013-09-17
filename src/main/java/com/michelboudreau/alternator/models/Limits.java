a5f27735b623737c8bea841c31a933bbc16b7b17
Michel Boudreau <michelboudreau@gmail.com>
2013-09-17 21:57:30 +1000

    fixing limits to be the proper number

--- src/main/java/com/michelboudreau/alternator/models/Limits.java
+++ src/main/java/com/michelboudreau/alternator/models/Limits.java
@@ -1,7 +1,7 @@
 package com.michelboudreau.alternator.models;
 
 public class Limits {
-	public static final int NUMBER_MAX = 10^+126;
-	public static final int NUMBER_MIN = 10^-128;
+	public static final int NUMBER_MAX = (int)Math.pow(10, 126);
+	public static final int NUMBER_MIN = (int)Math.pow(10, -128);
 	public static final int TABLE_MAX = 256;
 }
